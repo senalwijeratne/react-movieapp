4ae4f1b966a60da5623400686b6fab32274f0772
Senal Wijeratne <senal.ra128@gmail.com>
2018-10-27 13:38:10 +0530

    axios data retrieval from the API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import axios from "axios"
 
 import "./styles.css"
 
@@ -38,49 +39,72 @@ class SearchBox extends React.Component {
 class MovieList extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      popularIsLoaded: false
+    }
+
+    this.retrivePopular = this.retrivePopular.bind(this)
+  }
+
+  componentDidMount() {
+    this.retrivePopular()
+  }
+
+  retrivePopular() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/discover/movie?api_key=885130303f08da8840fcee905162aaac&language=en-US&sort_by=popularity.desc"
+      )
+      .then(response => {
+        this.setState({
+          popularMovieData: response.data.results.slice(0, 3),
+          popularIsLoaded: true
+        })
+      })
   }
 
   render() {
-    return (
-      <div className="movie-section">
-        <div className="movie-card">
-          <img
-            className="movie-card-image"
-            src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/faIB-hero-pie.jpg"
-            alt="hero of the movie"
-          />
-          <div className="movie-card-title">Movie Title</div>
-          <div className="movie-card-description">
-            Bacon ipsum dolor amet sausage brisket biltong kielbasa kevin pig drumstick boudin capicola burgdoggen doner
-            porchetta shank spare ribs.
+    if (this.state.popularIsLoaded) {
+      return (
+        <div className="movie-section">
+          <div className="movie-card">
+            <img
+              className="movie-card-image"
+              src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/faIB-hero-pie.jpg"
+              alt="hero of the movie"
+            />
+            <div className="movie-card-title">{this.state.popularMovieData[0].title}</div>
+            <div className="movie-card-description">{this.state.popularMovieData[0].overview}</div>
+          </div>
+          <div className="movie-card">
+            <img
+              className="movie-card-image"
+              src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/faIB-hero-pie.jpg"
+              alt="hero of the movie"
+            />
+            <div className="movie-card-title">{this.state.popularMovieData[1].title}</div>
+            <div className="movie-card-description">{this.state.popularMovieData[1].overview}</div>
+          </div>
+          <div className="movie-card">
+            <img
+              className="movie-card-image"
+              src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/faIB-hero-pie.jpg"
+              alt="hero of the movie"
+            />
+            <div className="movie-card-title">{this.state.popularMovieData[2].title}</div>
+            <div className="movie-card-description">{this.state.popularMovieData[2].overview}</div>
           </div>
         </div>
-        <div className="movie-card">
-          <img
-            className="movie-card-image"
-            src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/faIB-hero-pie.jpg"
-            alt="hero of the movie"
-          />
-          <div className="movie-card-title">Movie Title</div>
-          <div className="movie-card-description">
-            Bacon ipsum dolor amet sausage brisket biltong kielbasa kevin pig drumstick boudin capicola burgdoggen doner
-            porchetta shank spare ribs.
-          </div>
+      )
+    } else {
+      return (
+        <div className="spinner">
+          <div className="double-bounce1" />
+          <div className="double-bounce2" />
         </div>
-        <div className="movie-card">
-          <img
-            className="movie-card-image"
-            src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/faIB-hero-pie.jpg"
-            alt="hero of the movie"
-          />
-          <div className="movie-card-title">Movie Title</div>
-          <div className="movie-card-description">
-            Bacon ipsum dolor amet sausage brisket biltong kielbasa kevin pig drumstick boudin capicola burgdoggen doner
-            porchetta shank spare ribs.
-          </div>
-        </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
@@ -89,8 +113,7 @@ class MovieApp extends React.Component {
     super(props)
 
     this.state = {
-      searchTerm: "",
-      isLoaded: false
+      searchTerm: ""
     }
   }
 
